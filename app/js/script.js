2cbcf5590e048eecef56a84ece5e5995aab6ecd5
rubyhay <115424833+rubyeleanor@users.noreply.github.com>
2024-08-22 15:09:18 +0100

    Styling and JS
    Js added for nav bar

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+const openBtn = document.querySelector('#OpenBtn');
+const navMenu = document.querySelector('.top-nav-menu');
+const navLinks = document.querySelectorAll('.top-nav-link');
+
+
+openBtn.addEventListener('click', () => {
+    openBtn.classList.toggle('navbtn-open');
+    navMenu.classList.toggle('active');
+})
+
+navLinks.addEventListener('click', () => {
+    openBtn.classList.remove('navbtn-open');
+    navMenu.classList.remove('active');
+})
